0c0762a456b5d378b824326b60e1d41376d6dd8f
Jelly Choi <chm073@gmail.com>
2017-07-28 00:47:45 +0900

    Update db shema

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :presentations
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root to: "home#index"
 end
